7b49411c880a94a142ad2ffca940581b4d2e0719
Mick Soum <mick26soum@gmail.com>
2015-06-30 23:04:16 -0700

    first commit-new

---
+++ CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  RosterClass_Table_Scratch
+//
+//  Created by Mick Soumphonphakdy on 6/30/15.
+//  Copyright (c) 2015 Mick Soum. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  RosterClass_Table_Scratch
+//
+//  Created by Mick Soumphonphakdy on 6/30/15.
+//  Copyright (c) 2015 Mick Soum. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+   
+}
